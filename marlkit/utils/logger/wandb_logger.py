a6aacc34c660b144484225999b5c304fdb841657
성원(Sam) 홍 <hongsw94@gmail.com>
2026-02-24 17:48:34 +0900

    Modified WandB logger (to accumulate-and-flush pattern)

--- marlkit/utils/logger/wandb_logger.py
+++ marlkit/utils/logger/wandb_logger.py
@@ -2,28 +2,50 @@ import wandb
 from typing import Dict, Any 
 from .base_logger import BaseLogger 
 
-class WandBLogger(BaseLogger): 
-
-    def __init__(self, project: str, run_name: str, config: Dict[str, Any]): 
+class WandBLogger(BaseLogger):
+    
+    def __init__(self, 
+                 project: str, 
+                 run_name: str, 
+                 config: Dict[str, Any]): 
         super().__init__(project, run_name, config) 
         wandb.init(
             project=project, 
             name=run_name, 
-            config=config 
+            config=config, 
         )
-
+        self._step_buffer: Dict[str, Any] = {} 
+        self._current_step: int | None = None 
+    
+    def _flush(self): 
+        """Send accumulated metrics for the current step."""
+        if self._step_buffer and self._current_step is not None: 
+            wandb.log(self._step_buffer, step=self._current_step) 
+            self._step_buffer = {} 
+    
     def log_scalar(self, tag: str, value: float, step: int): 
-        wandb.log({tag: value}, step=step) 
+        if self._current_step is not None and step != self._current_step:
+            self._flush() 
+        self._current_step = step 
+        self._step_buffer[tag] = value 
     
     def log_scalars(self, tag: str, values: Dict[str, float], step: int): 
-        wandb.log({f"{tag}/{k}": v for k, v in values.items()}, step=step)
+        if self._current_step is not None and step != self._current_step: 
+            self._flush() 
+        self._current_step = step 
+        self._step_buffer.update({f"{tag}/{k}": v for k, v in values.items()}) 
 
     def log_histogram(self, tag: str, values, step: int): 
-        wandb.log({tag: wandb.Histogram(values)}, step=step) 
-
+        if self._current_step is not None and step != self._current_step: 
+            self._flush() 
+        self._current_step = step 
+        self._step_buffer[tag] = wandb.Histogram(values) 
+    
     def log_config(self): 
-        pass  # Already handled by wandb.init(config=...)
+        pass 
 
     def close(self): 
+        self._flush() 
         wandb.finish() 
+        
 
